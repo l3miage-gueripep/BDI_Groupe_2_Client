2707641e0d47de2e3e09e60cea37d8e12583d877
paul <gueripel.paul@gmail.com>
2024-01-22 11:52:05 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+
+@Component({
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+	constructor(private appService: AppService) { }
+
+	title = 'angular-nodejs-example';
+
+	person: any;
+    
+    connected = false;
+
+	ngOnInit() {
+		this.appService.getPerson()
+			.subscribe((person: any) => {
+				this.person = person;
+                this.connected = true;
+			});
+	}
+}
