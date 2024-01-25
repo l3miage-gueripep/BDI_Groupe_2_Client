e7ada986e79e00388d3ab27212b619318e056e8c
l3miage-xusi <121896780+l3miage-xusi@users.noreply.github.com>
2024-01-25 09:10:48 +0100

    polices

--- src/app/authentication/user/user.component.ts
+++ src/app/authentication/user/user.component.ts
@@ -4,7 +4,7 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.scss']
 })
 export class UserComponent {
   constructor(private firebaseService: FirebaseService) { 
