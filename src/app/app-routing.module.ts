b027fcb743e397ba613b1905c4b1815c27f91e5d
l3miage-xusi <121896780+l3miage-xusi@users.noreply.github.com>
2024-01-29 11:48:36 +0100

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,9 +12,11 @@ import { LoginComponent } from './authentication/login/login.component';
 import { PanierPageComponent } from './panier/panier-page/panier-page.component';
 import { ReservationCovoiturageComponent } from './reservation/reservation-covoiturage/reservation-covoiturage.component';
 import { ReservationReussieComponent } from './reservation/reservation-reussie/reservation-reussie.component';
+import {HomePageComponent} from "./home-page/home-page/home-page.component";
 
 
 const routes: Routes = [
+  { path: '', component: HomePageComponent },
   { path: 'register', component: RegisterComponent },
   { path:'recherche', component: ResultSearchPageComponent },
   { path:'recherche/covoiturage', component: CovoiturageRecherchePageComponent},
---
+++ src/app/home-page/home-page/home-page.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {MatButton} from "@angular/material/button";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+    imports: [
+        MatButton
+    ],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+  constructor( private router: Router) {
+  }
+  navigateToRecherche() {
+    this.router.navigate(['/recherche'], { queryParams: { query: '' } });
+  }
+}
