39a3b02042c10ff1e2877c624de6ea80c6cbc9bd
l3miage-oulahcel <oulahcel@etu.univ-grenoble-alpes.fr>
2024-01-31 15:49:11 +0100

    imagesFestival

--- src/app/resultat-recherche/resultat-card/resultat-card.component.ts
+++ src/app/resultat-recherche/resultat-card/resultat-card.component.ts
@@ -18,4 +18,37 @@ export class ResultatCardComponent {
     navigateToCovoiturage() {
         this.router.navigate(['recherche/covoiturage'], { queryParams: { query: this.festivalName } });
     }
+
+    getImagePath() {
+      switch (this.nomDomaine) {
+            case 'Musiques classiques':
+                return 'assets/musiqueClassique.jpg';
+            case 'Livre et littérature':
+                return 'assets/livreEtLitterature.jpg';
+            case 'Musiques actuelles' :
+                return 'assets/musiqueActuelle.jpg';
+            case 'Pluridisciplinaire Spectacle vivant' :
+                return 'assets/pluridisciplinaireSpectacle.jpg';
+            case 'Cirque et Arts de la rue' :
+                return 'assets/cirqueEtArts.jpg';
+            case'Divers Spectacle vivant' :
+                return 'assets/diversSpectacle.jpg';
+            case 'Transdisciplinaire' :
+                return 'assets/transdisciplinaire.jpg';
+            case 'Cinéma et audiovisuel' :  
+                return 'assets/cinemaEtAudiovisuel.jpg';
+            case 'Pluridisciplinaire Musique' : 
+                return 'assets/pluridisciplinaireMusique.jpg';
+            case 'Danse' :
+                return 'assets/danse.jpg';
+            case 'Arts plastiques et visuels' :
+                return 'assets/artsPlastiques.jpg';
+            case 'Domaines divers'  :
+                return 'assets/domainesDivers.jpg';
+            case 'Théâtre' :
+                return 'assets/theatre.jpg';
+            default:
+                return 'assets/billet.jpg';
+      }
+  }
 }
